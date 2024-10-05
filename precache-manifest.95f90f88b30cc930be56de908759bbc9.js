30c2359a56a53906a62b0ff5e86727b76099de7c
Aguru Sireesha <agurusireeshaedu@gmail.com>
2024-10-05 11:48:18 +0530

    Updates

---
+++ precache-manifest.95f90f88b30cc930be56de908759bbc9.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fec852a9743bfda8a590669db1cc668a",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "33b337982867f0ae2ff8",
+    "url": "/my-portfolio/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "22ed7d83510fddd953aa",
+    "url": "/my-portfolio/static/css/main.0f0d2f7f.chunk.css"
+  },
+  {
+    "revision": "33b337982867f0ae2ff8",
+    "url": "/my-portfolio/static/js/2.833aadba.chunk.js"
+  },
+  {
+    "revision": "ab0f0f50a816193d1c7b",
+    "url": "/my-portfolio/static/js/3.8b0869b4.chunk.js"
+  },
+  {
+    "revision": "22ed7d83510fddd953aa",
+    "url": "/my-portfolio/static/js/main.0faaf391.chunk.js"
+  },
+  {
+    "revision": "d6a32fec32a4bf52c0b6",
+    "url": "/my-portfolio/static/js/runtime~main.c8052818.js"
+  },
+  {
+    "revision": "2d37cd8c7e36ffcd9e360d0cece5dbdf",
+    "url": "/my-portfolio/static/media/about.2d37cd8c.jpg"
+  },
+  {
+    "revision": "d5ac04070ea5102ab5f2ed893255a0cf",
+    "url": "/my-portfolio/static/media/ai .d5ac0407.png"
+  },
+  {
+    "revision": "4a07517930a50ec7595ea880716e2306",
+    "url": "/my-portfolio/static/media/banner.4a075179.png"
+  },
+  {
+    "revision": "d1df084dff45b6c207919e3890ce73de",
+    "url": "/my-portfolio/static/media/bright-light-black-background.d1df084d.jpg"
+  },
+  {
+    "revision": "f2736b1dd74bc001f3ee953b73c871fb",
+    "url": "/my-portfolio/static/media/btech.f2736b1d.jpg"
+  },
+  {
+    "revision": "525969393ddd0f6db32c4f5967faf9ff",
+    "url": "/my-portfolio/static/media/chatbot.52596939.png"
+  },
+  {
+    "revision": "b83d97d08674cab08eb2063ac36a49d5",
+    "url": "/my-portfolio/static/media/covid19dashboard.b83d97d0.png"
+  },
+  {
+    "revision": "b5585705c3d95e67c1f94b5de2e7d828",
+    "url": "/my-portfolio/static/media/gorest.b5585705.png"
+  },
+  {
+    "revision": "87c65ab4480006ea01975b8463277b23",
+    "url": "/my-portfolio/static/media/jobby.87c65ab4.png"
+  },
+  {
+    "revision": "9cd9267c9ac272009d3607ff10b1312a",
+    "url": "/my-portfolio/static/media/logo.9cd9267c.png"
+  },
+  {
+    "revision": "df86c703f31a2796f4c270832120f094",
+    "url": "/my-portfolio/static/media/mydashboard.df86c703.png"
+  },
+  {
+    "revision": "745640c1fd0970462c1a01d63d931e7b",
+    "url": "/my-portfolio/static/media/nxttrendz.745640c1.png"
+  },
+  {
+    "revision": "1dd96e702d84e396e0a342608d7c8d8d",
+    "url": "/my-portfolio/static/media/nxtwatch.1dd96e70.png"
+  },
+  {
+    "revision": "db26cdf7b17e813e99aebd54ad336505",
+    "url": "/my-portfolio/static/media/priceofdiamond.db26cdf7.png"
+  },
+  {
+    "revision": "f8c4313f263ee34fb593cff0376af3c3",
+    "url": "/my-portfolio/static/media/sireesha.f8c4313f.jpg"
+  },
+  {
+    "revision": "a53c1fc08786f9f55fc2bf6ab8adc30a",
+    "url": "/my-portfolio/static/media/skills.a53c1fc0.jpg"
+  }
+]);
