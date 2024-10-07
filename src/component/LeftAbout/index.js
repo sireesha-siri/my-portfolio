7a40ad468b550798fce537bc2c42e84fb637f32a
Aguru Sireesha <agurusireeshaedu@gmail.com>
2024-10-07 13:24:05 +0530

    Update index.js

--- src/component/LeftAbout/index.js
+++ src/component/LeftAbout/index.js
@@ -5,7 +5,7 @@ import sireesha from '../../images/sireesha.jpg'
 import './index.css'
 
 const LeftAbout = () => (
-    <>
+    <div>
         <div className="image-container">
                 <img src={sireesha} alt='siri' className="my-image"/>
         </div>
@@ -13,7 +13,7 @@ const LeftAbout = () => (
         <div className="image-container-for-small">
             <img src={sireesha} alt='siri' className="my-image-for-small"/>
         </div>
-</>
+</div>
 )
 
 export default LeftAbout
